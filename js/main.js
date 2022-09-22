e58c4d41b8bf3a0039286d5d2bf7669a96b7ec9c
Koralek <dkoralek2@gmail.com>
2022-09-22 11:39:56 +0200

    skończona nawigacja

--- js/main.js
+++ js/main.js
@@ -10,6 +10,17 @@ const handleNav = () => {
             nav.classList.remove('nav--active')
         })
     })
+    handeNavItemsAnimation();
+}
+
+const handeNavItemsAnimation = () => {
+    let delayTime = 0;
+
+    allNavItems.forEach(item => {
+        item.classList.toggle('nav-items-animation')
+        item.style.animationDelay = '.' + delayTime + 's';
+        delayTime++;
+    })
 }
 
 navBtn.addEventListener('click', handleNav)
